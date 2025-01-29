bb02d9e0593ccfa06974f810cd3689ece677314a
Piyush <piyush@ibarts.in>
2025-01-29 05:47:32 +0530

    otp

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,7 +1,8 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { Welcome } from "../welcome/welcome.js";
+import { Navbar } from "../component/navbar.jsx";
 
-export function meta({}: Route.MetaArgs) {
+export function meta({ }: Route.MetaArgs) {
   return [
     { title: "New React Router App" },
     { name: "description", content: "Welcome to React Router!" },
@@ -9,5 +10,8 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Welcome />;
+  return
+
+    <Welcome />;
+
 }
